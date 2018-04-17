8c1fbadea49c3a5b423620894b67cb5f1d04429f
sabrina palm <sabrina_palm@hotmail.com>
2018-04-17 22:27:33 +0200

    orkar inte mer

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-window.addEventListener('load', () => {
+/*
+
+input-fält för att mata in ett tal
+presentation av resultatet hittills
+knappar för vanliga räknesätt (+, -, *, /)
+knapp för att rensa resultatet och återställa resultatet till noll (C, Clear eller motsvarande)
+knapp för att räkna ut ett resultat (=)
+appen ska ha en tydlig design (förklara för användaren hur man förväntas använda den, användaren ska inte behöva gissa vad en knapp gör)
+appen ska versionshanteras med Git och vara publicerad på GitHub
+
+*/window.addEventListener('load', () => {
     
     //Make new Vue object
 	new Vue({
@@ -8,29 +18,45 @@ window.addEventListener('load', () => {
         
         //Define data
 		data: {
-			result: '',
-			num: '',
-			operator: '',
-			disableButton: true,
-			listNum: [],
+            display: '',
+            currentNumber: '',
+            lastNumber: [],
+            result : '',
+            operator: ''
 		},
         
         //Define methods/functions
 		methods: {
             
             //Get number values (REWRITE THIS FUNCTION CAUSE IT SUX)
-			value: function (event) {
-                this.num += event.target.innerHTML;
-                this.result = this.num;
+			value: function (value) {
+                
+                this.currentNumber += value;
+                
+                this.display = this.currentNumber;
+            
 			},
-            //Evaluate numbers
-			eval: function (event) {
-				this.result = eval(this.num);
-                Math.round(this.result);
-                this.listNum.push(`${this.num} ${event.target.innerHTML} ${this.result}`);
-				console.log(this.listNum)
-				this.num = '';
-			},
+            
+            opp: function(value) {
+                
+                if(value === "+") {
+                    
+                    this.lastNumber.push(this.currentNumber);
+                    
+                    this.currentNumber = '';
+                    
+                    this.display = this.currentNumber;
+                    
+                    console.log(this.lastNumber);
+
+                }
+
+            },
+            
+            equal: function() {
+                
+                
+            },
             //Single function raised in two
 			raisedInTwo: function () {
 				this.result = Math.pow(this.result, 2)
